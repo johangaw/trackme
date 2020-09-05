8ac59188b4c520171c5dfdc750e89ec26f9834b6
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-09-05 14:38:56 +0200

    Track location in foreground service

--- app/src/main/java/com/example/trackme/LocationTrackerService.kt
+++ app/src/main/java/com/example/trackme/LocationTrackerService.kt
@@ -2,7 +2,10 @@ package com.example.trackme
 
 import android.annotation.SuppressLint
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.os.Looper
@@ -13,10 +16,12 @@ import com.google.android.gms.location.*
 
 class LocationTrackerService : Service() {
 
-    val CHANNEL_ID = "ForegroundServiceChannel"
+    val CHANNEL_ID = "LocationTrackerServiceChannel"
+    val CHANNEL_NAME = "LocationTrackerServiceChannel"
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var tracking = false
+    private val TAG: String? = this::class.simpleName
 
 
     override fun onCreate() {
@@ -25,44 +30,64 @@ class LocationTrackerService : Service() {
 
     }
 
-
+    @SuppressLint("MissingPermission")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val res = super.onStartCommand(intent, flags, startId)
-
+        super.onStartCommand(intent, flags, startId)
 
         if(!tracking) {
             tracking = true
 
-            val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("LocationTrackerService")
-                .build()
-            startForeground(1, notification)
-
-            val locationRequest = LocationRequest().apply {
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            }
-
-            val locationCallback = object : LocationCallback() {
-                override fun onLocationResult(locationResult: LocationResult?) {
-                    locationResult ?: return
-                    for (location in locationResult.locations){
-                        Log.d(this::class.simpleName, location.toString())
-                    }
-                }
-            }
+            showTrackingNotification()
 
             fusedLocationClient.requestLocationUpdates(
-                locationRequest,
-                locationCallback,
+                createLocationRequest(),
+                createLocationCallback(),
                 Looper.getMainLooper()
             )
-
         }
 
-        return res
+        return START_STICKY
     }
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    private fun createLocationCallback(): LocationCallback {
+        return object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    Log.d(TAG, location.toString())
+                }
+            }
+        }
+    }
+
+    private fun createLocationRequest(): LocationRequest? {
+        return LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private fun showTrackingNotification() {
+        val chan = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_NONE
+        )
+//        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val manager = (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+        manager.createNotificationChannel(chan)
+
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setOngoing(true)
+            .setContentTitle("LocationTrackerService")
+            .setCategory(Notification.CATEGORY_NAVIGATION)
+            .build()
+        startForeground(1, notification)
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 }
