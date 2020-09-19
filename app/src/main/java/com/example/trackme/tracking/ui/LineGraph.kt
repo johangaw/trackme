c0d2d1569a0846fc08972f590f2bce4d2d6efdf3
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-09-19 21:27:15 +0200

    Refactor line drawing

--- app/src/main/java/com/example/trackme/tracking/ui/LineGraph.kt
+++ app/src/main/java/com/example/trackme/tracking/ui/LineGraph.kt
@@ -9,7 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.ui.tooling.preview.Devices
 import androidx.ui.tooling.preview.Preview
 
@@ -34,35 +36,35 @@ fun LineGraph(
     modifier: Modifier,
     data: List<Point>,
 ) {
-
     val xMin = data.minOf { it.x }
     val xMax = data.maxOf { it.x }
     val yMin = data.minOf { it.y }
     val yMax = data.maxOf { it.y }
 
     Canvas(modifier = modifier.background(Color.White)) {
-        val xInter = Interpolation(xMin - xMax * 0.05f, xMax * 1.1f,  0f, size.width)
+        val xInter = Interpolation(xMin - xMax * 0.05f, xMax * 1.1f, 0f, size.width)
         val yInter = Interpolation(yMin - yMax * 0.05f, yMax * 1.1f, size.height, 0f)
 
         drawOriginLines(this, xInter, yInter)
 
         val color = Color.Red
-        data.zipWithNext{p1, p2 ->
-            val x1 = xInter.interpolate(p1.x)
-            val y1 = yInter.interpolate(p1.y)
-            val x2 = xInter.interpolate(p2.x)
-            val y2 = yInter.interpolate(p2.y)
-            drawLine(color, Offset(x1, y1), Offset(x2, y2))
-        }
 
-        data.forEach {p ->
+        val path = Path()
+        data.forEachIndexed { index, point ->
+            val x = xInter.interpolate(point.x)
+            val y = yInter.interpolate(point.y)
+            if (index == 0) path.moveTo(x, y) else path.lineTo(x, y)
+        }
+        drawPath(path, color, style = Stroke())
+
+        data.forEach { p ->
             val center = Offset(xInter.interpolate(p.x), yInter.interpolate(p.y))
             drawCircle(color, 15f, center)
         }
     }
 }
 
-fun drawOriginLines(drawScope: DrawScope, xInter: Interpolation, yInter: Interpolation ) {
+fun drawOriginLines(drawScope: DrawScope, xInter: Interpolation, yInter: Interpolation) {
     drawScope.apply {
         val xOrigin = xInter.interpolate(0f)
         val yOrigin = yInter.interpolate(0f)
