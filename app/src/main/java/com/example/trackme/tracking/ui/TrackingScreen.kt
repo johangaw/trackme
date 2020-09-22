e957051aee6c16ae57c60dcfd5594457a7e24bad
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-09-22 20:40:24 +0200

    Use LineGraph on tracking screen

--- app/src/main/java/com/example/trackme/tracking/ui/TrackingScreen.kt
+++ app/src/main/java/com/example/trackme/tracking/ui/TrackingScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Devices
 import androidx.ui.tooling.preview.Preview
+import com.example.trackme.data.TrackEntry
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -22,7 +25,11 @@ fun TrackingScreen(
     startedAt: LocalDateTime?,
     totalLength: Float,
     currentSpeed: Double,
+    trackEntries: List<TrackEntry>,
 ) {
+
+    val speedPoints =
+        remember(trackEntries) { trackEntries.map { Point(it.time.toFloat(), it.speed) } }
 
     if (startedAt == null) {
         Column(
@@ -52,6 +59,10 @@ fun TrackingScreen(
                 Text(text = formatDistance(totalLength), style = MaterialTheme.typography.h3)
                 Text(text = "$currentSpeed m/s", style = MaterialTheme.typography.h3)
             }
+            LineGraph(
+                modifier = Modifier.fillMaxWidth().preferredHeight(200.dp),
+                data = speedPoints,
+            )
             Row(
                 modifier = Modifier.fillMaxSize(),
                 horizontalArrangement = Arrangement.Center,
@@ -93,7 +104,8 @@ fun TrackingScreenPreview() {
             onStopClick = {},
             startedAt = trackStartedAt,
             totalLength = 1337F,
-            currentSpeed = 3.67
+            currentSpeed = 3.67,
+            trackEntries = trackEntries,
         )
     }
 }
@@ -112,6 +124,32 @@ fun NotTrackingScreenPreview() {
             startedAt = null,
             totalLength = 0F,
             currentSpeed = 0.0,
+            trackEntries = emptyList()
         )
     }
 }
+
+fun createEntry(time: Long, speed: Float): TrackEntry =
+    TrackEntry(time = time,
+               trackId = 0,
+               latitude = 0.0,
+               longitude = 0.0,
+               bearing = 0F,
+               speed = speed,
+               altitude = 0.0,
+               id = 0)
+
+val trackEntries = listOf<TrackEntry>(
+    createEntry(0, 3f),
+    createEntry(1, 4f),
+    createEntry(2, 5f),
+    createEntry(3, 4f),
+    createEntry(6, 3f),
+    createEntry(9, 3f),
+    createEntry(11, 3.5f),
+    createEntry(14, 3.6f),
+    createEntry(16, 3.7f),
+    createEntry(17, 4f),
+    createEntry(18, 5f),
+    createEntry(19, 5.5f),
+)
