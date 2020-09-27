0ec9bef9bfb060d8dd038c3b4839c025fc4a8613
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-09-27 13:05:49 +0200

    Clock updated when pausing the track

--- app/src/main/java/com/example/trackme/tracks/ui/TracksScreen.kt
+++ app/src/main/java/com/example/trackme/tracks/ui/TracksScreen.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Device
+import androidx.ui.tooling.preview.Devices
+import androidx.ui.tooling.preview.Preview
 import com.example.trackme.data.Track
 import com.example.trackme.tracking.ui.RoundTextButton
 
@@ -18,13 +21,13 @@ fun TracksScreen(
 ) {
     Column(modifier = Modifier.fillMaxSize()) {
         for (track in tracks) {
-            Row(modifier = Modifier.fillMaxWidth().clickable(onClick = {onTrackClick(track)})) {
+            Row(modifier = Modifier.fillMaxWidth().clickable(onClick = { onTrackClick(track) })) {
                 Text(track.id.toString(), modifier = Modifier.preferredWidth(24.dp))
                 Text(track.name)
             }
         }
 
-        if(tracks.count() == 0) {
+        if (tracks.count() == 0) {
             RoundTextButton(
                 modifier = Modifier.gravity(Alignment.CenterHorizontally),
                 onClick = onNewClick,
@@ -32,4 +35,18 @@ fun TracksScreen(
             )
         }
     }
+}
+
+
+@Composable
+@Preview(device = Devices.PIXEL_3)
+fun TracksScreenPreview() {
+    val tracks: List<Track> = listOf(
+        Track(1, "Track 1", false),
+        Track(1, "Track 2", false),
+        Track(1, "Track 3", false),
+        Track(1, "Track 4", false),
+    )
+    TracksScreen(tracks = tracks, onTrackClick = {}, onNewClick = {})
+
 }
