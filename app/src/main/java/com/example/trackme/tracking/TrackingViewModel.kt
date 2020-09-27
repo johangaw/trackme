b1968719984170e028f02ca70dba360eb0341341
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-09-27 18:51:29 +0200

    Moved track status update to LocationService

--- app/src/main/java/com/example/trackme/tracking/TrackingViewModel.kt
+++ app/src/main/java/com/example/trackme/tracking/TrackingViewModel.kt
@@ -15,30 +15,21 @@ class TrackingViewModel(
 
     val activeTrackEntries: LiveData<List<TrackEntry>> =
         database.trackEntryDao().getAllAndObserve(trackId)
+
     val trackStartedAt: LiveData<LocalDateTime?> = Transformations.map(activeTrackEntries) {
         it.firstOrNull()?.time?.let { timeStamp ->
             LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.systemDefault())
         } ?: LocalDateTime.now()
     }
+
     val totalDistance: LiveData<Float> =
         Transformations.map(activeTrackEntries) {
             it?.map { entry -> entry.asLocation() }
                 ?.zipWithNext { l1, l2 -> l1.distanceTo(l2) }
                 ?.sum() ?: 0F
         }
+
     val activeTrack: LiveData<Track> = database.trackDao().getAndObserve(trackId)
-
-    fun startTracking() {
-        viewModelScope.launch {
-            database.trackDao().updateActivity(TrackActivity(trackId, true))
-        }
-    }
-
-    fun stopTracking() {
-        viewModelScope.launch {
-            database.trackDao().updateActivity(TrackActivity(trackId, false))
-        }
-    }
 }
 
 @Suppress("UNCHECKED_CAST")
