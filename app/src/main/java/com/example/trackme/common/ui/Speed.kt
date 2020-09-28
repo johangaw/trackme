3123f37366cef42f3ba265e397f7c1086aa0cf77
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2020-09-28 16:20:50 +0200

    Use two decimals when displaying speed

--- app/src/main/java/com/example/trackme/common/ui/Speed.kt
+++ app/src/main/java/com/example/trackme/common/ui/Speed.kt
@@ -5,12 +5,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import kotlin.math.roundToInt
 
+/**
+ * @param speed in m/s
+ */
 @Composable
 fun Speed(
     speed: Float,
     style: TextStyle = MaterialTheme.typography.body1,
     modifier: Modifier = Modifier,
 ) {
-    Text(text = "$speed m/s", style = style, modifier = modifier)
+    val roundedSpeed = (speed * 100).roundToInt() / 100f
+    Text(text = "$roundedSpeed m/s", style = style, modifier = modifier)
 }
