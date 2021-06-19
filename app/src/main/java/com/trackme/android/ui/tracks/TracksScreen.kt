86d6924b8083ba91820e41a957acda2917f8f372
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2021-06-19 15:25:29 +0200

    Fix: Close side slide when deleting item

--- app/src/main/java/com/trackme/android/ui/tracks/TracksScreen.kt
+++ app/src/main/java/com/trackme/android/ui/tracks/TracksScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -47,11 +48,14 @@ fun TracksScreen(
     ) {
         Column(Modifier.verticalScroll(rememberScrollState())) {
             tracks.forEach { track ->
-                TrackRow(
-                    track = track,
-                    onSelect = onTrackClick,
-                    onDelete = onTrackDelete,
-                )
+                key(track.id) {
+                    TrackRow(
+                        track = track,
+                        onSelect = onTrackClick,
+                        onDelete = onTrackDelete,
+                    )
+
+                }
             }
         }
     }
@@ -63,7 +67,7 @@ fun TrackRow(
     track: TrackData,
     onSelect: (TrackData) -> Unit,
     onDelete: (TrackData) -> Unit,
-    swipeableState: SwipeableState<String> = rememberSwipeableState("hidden")
+    swipeableState: SwipeableState<String> = rememberSwipeableState("hidden"),
 ) {
     val anchors = mapOf(0f to "hidden", 300f to "visible")
     Box(
