121e70ec5ab032562fa0d894036d7a17cd321ddc
Johan Gustavsson <johan.bo.gustavsson@gmail.com>
2021-06-19 17:08:15 +0200

    Make sure to show the same distance everywhere

--- app/src/main/java/com/trackme/android/data/helpers.kt
+++ app/src/main/java/com/trackme/android/data/helpers.kt
@@ -1,7 +1,9 @@
 package com.trackme.android.data
 
 fun totalDistance(entries: List<TrackEntry>): Float {
-    return entries.map { entry -> entry.asLocation() }
+    return entries
+        .sortedBy { it.time }
+        .map { entry -> entry.asLocation() }
         .zipWithNext { l1, l2 -> l1.distanceTo(l2) }
         .sum()
 }
